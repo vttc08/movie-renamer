60e2272bc50bc2dfe7333a61029c4aa0d6df0a86
Kevin Hu <hukevin69@gmail.com>
2024-01-09 10:42:16 -0800

    minor bug fix with thunar crashing and progress.txt persisting in movie dir after move is finished

--- api.sh
+++ api.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+
 fullpath=/mnt$1
 source /config/movie-rename-script/.env
 # source .env # if run locally
-touch -d "2 seconds ago" "$1"/* # set the modified time of the file to 2 seconds ago
 
 # If the file is not in /mnt/data, move it to /mnt/data/nzbget (temp dir for processing)
 # the script is execute in the POV of webtop where /data is /mnt/data on the host and /data/nzbget is /mnt/data/nzbget on the host
 if [[ "$fullpath" != /mnt/data* ]]; then
     # Move the folder with progress into /mnt/data/nzbget
     # define variables
+    touch -d "2 seconds ago" "$1"/* # set the modified time of the file to 2 seconds ago
     basename=$(basename "$1")
     start=$(date +%s)
     touch progress.txt # progress.txt is used in thunar to display progress
     # rsync copy with progress
-    rsync -a --progress "$1" "/data/nzbget/" & 
+    rsync -a --progress --exclude 'progress*.txt' "$1" "/data/nzbget/" & 
     pid=$! # declare pid of rsync
     trap "kill $pid 2> /dev/null" EXIT
     # While copy is running...
@@ -27,7 +28,6 @@ if [[ "$fullpath" != /mnt/data* ]]; then
         mv progress* "progress $progress-$rate.txt" # create an ever updating text file with progress
         sleep 1
     done
-
     # remove residual files
     rm progress*.txt
     rm -rf "$1"
--- mover.sh
+++ mover.sh
@@ -6,4 +6,4 @@ bind_dir=~/docker/webtop/config/movie-rename-script/ # the bind mount (host dir)
 # copy the .env file into the script dir
 cp .env $bind_dir/.env
 # copy the api.sh file into the script dir
-cp api.sh $bind_dir/api.sh
+cp api.sh $bind_dir/script.sh
