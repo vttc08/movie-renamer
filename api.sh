bd2537c8182395f5c57f8370eb5ed8ac2515300f
Kevin Hu <hukevin69@gmail.com>
2024-03-18 01:18:15 -0700

    changed api.sh for use in new webtop (openbox with spacefm)

--- api.sh
+++ api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fullpath=/mnt$1
+fullpath=$1
 source /config/movie-rename-script/.env
 # source .env # if run locally
 
@@ -16,14 +16,14 @@ if [[ "$fullpath" != /mnt/data* ]]; then
     start=$(date +%s)
     touch progress.txt # progress.txt is used in thunar to display progress
     # rsync copy with progress
-    rsync -a --progress --exclude 'progress*.txt' "$1" "/data/nzbget/" & 
+    rsync -a --progress --exclude 'progress*.txt' "$1" "/mnt/data/nzbget/" & 
     pid=$! # declare pid of rsync
     trap "kill $pid 2> /dev/null" EXIT
     # While copy is running...
     while kill -0 $pid 2> /dev/null; do
         basename=$(basename "$1")
         source_size=$(du -ks "$1" | awk '{print $1}')
-        target_size=$(du -s "/data/nzbget/$basename" | awk '{print $1}')
+        target_size=$(du -s "/mnt/data/nzbget/$basename" | awk '{print $1}')
         progress=$(printf "%.1f%%" $(echo "scale=3; $target_size/$source_size*100" | bc 2>/dev/null))
         seconds=$(($(date +%s) - $start))
         rate=$(printf "%.2f MBps" $(echo "scale=3; $target_size/$seconds/1024" | bc 2>/dev/null))
@@ -36,8 +36,9 @@ if [[ "$fullpath" != /mnt/data* ]]; then
     # Disable the trap on a normal exit.
     trap - EXIT
     # Olivetin processing of the files in nzbget folder
-    new_path="/mnt/data/nzbget/$basename"
-    curl -X POST "$OLIVETIN_URL/api/StartAction" -d "$data"
+    newpath="/mnt/data/nzbget/$basename"
+    newdata=$(jq -n --arg path '"'"$newpath"'"' '{"actionName": "Rename Movies", "arguments": [{"name": "path", "value": $path}]}')
+    curl -X POST "$OLIVETIN_URL/api/StartAction" -d "$newdata"
 else
     curl -X POST "$OLIVETIN_URL/api/StartAction" -d "$data"
 fi
