79e190ab7381df3431240433732ddab8c0ffbefe
Kevin Hu <hukevin69@gmail.com>
2024-03-18 01:38:58 -0700

    removed the txtfile progress bar indicator and trap function as spacefm allow the custom action to be run in a terminal which is able to display the progress bar

--- api.sh
+++ api.sh
@@ -4,38 +4,18 @@ fullpath=$1
 source /config/movie-rename-script/.env
 # source .env # if run locally
 
-# Create the JSON payload
+# Create the JSON payload if file is in /mnt/data
 data=$(jq -n --arg path '"'"$fullpath"'"' '{"actionName": "Rename Movies", "arguments": [{"name": "path", "value": $path}]}')
-# If the file is not in /mnt/data, move it to /mnt/data/nzbget (temp dir for processing)
-# the script is execute in the POV of webtop where /data is /mnt/data on the host and /data/nzbget is /mnt/data/nzbget on the host
+
+# If the file is not in /mnt/data (eg. it's on another drive), move it to /mnt/data/nzbget (temp dir for processing)
 if [[ "$fullpath" != /mnt/data* ]]; then
     # Move the folder with progress into /mnt/data/nzbget
     # define variables
-    touch -d "2 seconds ago" "$1"/* # set the modified time of the file to 2 seconds ago
+    touch -d "2 seconds ago" "$1"/* # update the modified time since these files are not modified by nzbget
     basename=$(basename "$1")
-    start=$(date +%s)
-    touch progress.txt # progress.txt is used in thunar to display progress
-    # rsync copy with progress
-    rsync -a --progress --exclude 'progress*.txt' "$1" "/mnt/data/nzbget/" & 
-    pid=$! # declare pid of rsync
-    trap "kill $pid 2> /dev/null" EXIT
-    # While copy is running...
-    while kill -0 $pid 2> /dev/null; do
-        basename=$(basename "$1")
-        source_size=$(du -ks "$1" | awk '{print $1}')
-        target_size=$(du -s "/mnt/data/nzbget/$basename" | awk '{print $1}')
-        progress=$(printf "%.1f%%" $(echo "scale=3; $target_size/$source_size*100" | bc 2>/dev/null))
-        seconds=$(($(date +%s) - $start))
-        rate=$(printf "%.2f MBps" $(echo "scale=3; $target_size/$seconds/1024" | bc 2>/dev/null))
-        mv progress* "progress $progress-$rate.txt" # create an ever updating text file with progress
-        sleep 1
-    done
-    # remove residual files
-    rm progress*.txt
-    rm -rf "$1"
-    # Disable the trap on a normal exit.
-    trap - EXIT
-    # Olivetin processing of the files in nzbget folder
+    rsync -a --progress --exclude 'progress*.txt' "$1" "/mnt/data/nzbget/" 
+    rm -rf "$1" # dangerous
+    # Olivetin processing of the files in nzbget folder, new JSON payload is created with the nzbget path
     newpath="/mnt/data/nzbget/$basename"
     newdata=$(jq -n --arg path '"'"$newpath"'"' '{"actionName": "Rename Movies", "arguments": [{"name": "path", "value": $path}]}')
     curl -X POST "$OLIVETIN_URL/api/StartAction" -d "$newdata"
